145c52e23dfe867a7c79888334080c69f07da38b
yusufnull <yusufrazak2025@gmail.com>
2025-10-08 11:47:27 +0800

    Changed schema

--- scripts/current-schema.sql
+++ scripts/current-schema.sql
@@ -129,6 +129,7 @@ CREATE TABLE public.user_credits (
   reserved_bigint bigint NOT NULL DEFAULT 0,
   updated_at timestamp with time zone DEFAULT now(),
   unlimited_credits_expiry timestamp with time zone NOT NULL DEFAULT '2025-10-03 12:00:00+00'::timestamp with time zone,
+  Paid_or_unpaid boolean DEFAULT false,
   CONSTRAINT user_credits_pkey PRIMARY KEY (user_id),
   CONSTRAINT user_credits_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id)
 );
